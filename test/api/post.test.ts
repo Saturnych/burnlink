e2fe6c9724fa56455ac7774cbbb658ddcc824014
Denis <saturnych@gmail.com>
2025-07-11 15:03:16 +0300

    Update post.test.ts

--- test/api/post.test.ts
+++ test/api/post.test.ts
@@ -7,7 +7,19 @@ const TIMEOUT = 3000;
 const DEBUG = process.env.NODE_ENV !== 'production';
 
 if (DEBUG) config({ quiet: true });
-const { PUBLIC_APP_URL, PUBLIC_LINK_URI, PUBLIC_TOKEN_URI, PRIVATE_TOKEN_EMAIL } = process.env;
+const {
+	PUBLIC_APP_URL,
+	PUBLIC_LINK_URI,
+	PUBLIC_TOKEN_URI,
+	PRIVATE_TOKEN_EMAIL,
+	PRIVATE_VERCEL_APP_NAME = 'burnlink',
+	PRIVATE_VERCEL_TEAM_SLUG = null,
+	PRIVATE_VERCEL_TEAM_ID = null,
+	PRIVATE_VERCEL_USER_ID = null,
+	PRIVATE_VERCEL_PROJECT_ID = null,
+	PRIVATE_VERCEL_TOKEN = null,
+	SHA = null,
+} = process.env;
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
 test.describe.configure({ mode: 'serial' });
@@ -15,9 +27,31 @@ test.describe.configure({ mode: 'serial' });
 let page: Page;
 let link: string;
 
-test.beforeAll(async ({ browser }) => {
+test.beforeAll(async ({ browser, request }) => {
+	console.log('SHA:', SHA);
 	console.log('PUBLIC_LINK_URI:', PUBLIC_LINK_URI);
 	console.log('PUBLIC_TOKEN_URI:', PUBLIC_TOKEN_URI);
+	// https://vercel.com/docs/rest-api/reference/endpoints/deployments/list-deployments
+	const response = await request.get(`https://api.vercel.com/v6/deployments?teamId=${PRIVATE_VERCEL_TEAM_ID}`, {
+		headers: {
+			'Authorization': !!PRIVATE_VERCEL_TOKEN ? `Bearer ${PRIVATE_VERCEL_TOKEN}` : undefined,
+			'Content-Type': 'application/json',
+			'Accept': 'application/json'
+		}
+	});
+	expect(response.ok()).toBeTruthy();
+	expect(response.status()).toBe(200);
+	const result = await response.json();
+	if (result?.deployments?.length>0) {
+		const deployment: object = result.deployments[0];
+		if (DEBUG) console.log('deployment:', deployment);
+		console.log('deployment state:', deployment.state);
+		console.log('deployment githubCommitSha:', deployment.meta.githubCommitSha);
+		const date: Date = new Date(new Date().toISOString());
+		const spentSec: number = Math.round((date.getTime() - Number(deployment.created)) / 1000);
+		console.log('deployment date:', deployment.created, 'spent:', spentSec);
+	}
+
 	page = await browser.newPage();
 });
 
@@ -31,7 +65,7 @@ test('api post requests', async ({ request }) => {
 	const apiToken = await request.post(`${PUBLIC_APP_URL}${PUBLIC_TOKEN_URI}`, {
 		headers: {
 			'Content-Type': 'application/json',
-			Accept: 'application/json'
+			'Accept': 'application/json'
 		},
 		data: {
 			email: PRIVATE_TOKEN_EMAIL,
@@ -56,9 +90,9 @@ test('api post requests', async ({ request }) => {
 	await sleep(TIMEOUT);
 	const response = await request.post(`${PUBLIC_APP_URL}${PUBLIC_LINK_URI}`, {
 		headers: {
-			Authorization: !!token ? `Bearer ${token}` : undefined,
+			'Authorization': !!token ? `Bearer ${token}` : undefined,
 			'Content-Type': 'application/json',
-			Accept: 'application/json'
+			'Accept': 'application/json'
 		},
 		data: {
 			text: pkg.repository.url,
